8ffee44cb84e025f4f52f72567ba3dbd409390e4
Ethan <ethan.scott2@icloud.com>
2024-02-12 09:35:36 -0600

    Initial Commit

---
+++ assignment notebook 2/assignment_notebook_2App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_notebook_2App.swift
+//  assignment notebook 2
+//
+//  Created by Ethan Scott on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct assignment_notebook_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
